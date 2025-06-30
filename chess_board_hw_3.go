958de54833d354c97713c7e359b36b3c5a1adb79
Alina Pechak <a.pechak@scalablesolutions.io>
2025-06-30 15:19:10 +0400

    Another ver-s for chess board

--- chess_board_hw_3.go
+++ chess_board_hw_3.go
@@ -5,21 +5,52 @@ import (
 	"strings"
 )
 
+// version 1
+//func main() {
+//	var oneNumber int
+//	_, err := fmt.Scan(&oneNumber)
+//	if err != nil {
+//		oneNumber = 8 // default value
+//	}
+//
+//	secondNumber := oneNumber
+//	for i := 0; i < secondNumber; i++ {
+//		if i%2 == 0 {
+//			fmt.Printf(strings.Repeat("# ", secondNumber/2+1))
+//			fmt.Printf("\n")
+//		} else {
+//			fmt.Printf(strings.Repeat(" #", secondNumber/2))
+//			fmt.Printf("\n")
+//		}
+//	}
+//}
+
 func main() {
 	var oneNumber int
 	_, err := fmt.Scan(&oneNumber)
 	if err != nil {
-		oneNumber = 8 // default value if the number is not provided
+		oneNumber = 8 // default value
 	}
 
 	secondNumber := oneNumber
 	for i := 0; i < secondNumber; i++ {
-		if i%2 == 0 {
-			fmt.Printf(strings.Repeat(" #", secondNumber))
-			fmt.Printf("\n")
+		if oneNumber%2 != 0 {
+			if i%2 == 0 {
+				fmt.Printf(strings.Repeat("# ", secondNumber/2+1))
+				fmt.Printf("\n")
+			} else {
+				fmt.Printf(strings.Repeat(" #", secondNumber/2))
+				//fmt.Printf(strings.Repeat("# ", secondNumber/2)) // для обычного прямоугольника
+				fmt.Printf("\n")
+			}
 		} else {
-			fmt.Printf(strings.Repeat("# ", secondNumber))
-			fmt.Printf("\n")
+			if i%2 == 0 {
+				fmt.Printf(strings.Repeat("# ", secondNumber/2))
+				fmt.Printf("\n")
+			} else {
+				fmt.Printf(strings.Repeat(" #", secondNumber/2))
+				fmt.Printf("\n")
+			}
 		}
 	}
 }
